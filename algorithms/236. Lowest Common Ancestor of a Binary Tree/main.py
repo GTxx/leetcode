e92ff5e1fc33b18b2dd6f93b01721afe1915dacc
gtxx <xiongxiong1986@gmail.com>
2016-11-06 07:21:51 +0000

    solve 236

--- algorithms/236. Lowest Common Ancestor of a Binary Tree/main.py
+++ algorithms/236. Lowest Common Ancestor of a Binary Tree/main.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
@@ -21,26 +21,41 @@ class Solution(object):
             else:
                 if root.left:
                     left_path = find(root.left, node)
-                    if not left_path:
-                        return []
-                    else:
+                    if left_path:
                         return [root] + left_path
                 if root.right:
                     right_path = find(root.right, node)
-                    if not right_path:
-                        return []
-                    else:
+                    if right_path:
                         return [root] + right_path
                 return []
 
         p_path = find(root, p)
         q_path = find(root, q)
         idx = 0
-        while p_path[idx] == q_path[idx]:
-            idx += 1
+        while True:
+            try:
+                if p_path[idx] == q_path[idx]:
+                    idx += 1
+                else:
+                    break
+            except Exception:
+                break
         return p_path[idx-1]
 
+if __name__ == "__main__":
+    n1 = TreeNode(1)
+    n2 = TreeNode(2)
+    n1.left = n2
+    s = Solution()
+    res = s.lowestCommonAncestor(n1, n1, n2)
+    print res
 
-
+    n1 = TreeNode(1)
+    n2 = TreeNode(2)
+    n3 = TreeNode(3)
+    n1.left = n2
+    n1.right = n3
+    res = s.lowestCommonAncestor(n1, n2, n3)
+    print res
 
 
